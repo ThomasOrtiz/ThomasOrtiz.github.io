4f829f85c4252bfed530efba5ae0027a14b6660b
Thomas Ortiz <Thomas Ortiz>
2017-01-16 00:41:47 -0500

    Fixed Google Analytics

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,24 +23,21 @@ $(function() {
         }
     });
 
-    window.onorientationchange = function() { document.body.scrollTop = 0; };
-
-    
-
-    // Google Analytics Tracking
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-90372061-1', 'auto');
-    ga('send', 'pageview');
-    
+    window.onorientationchange = function() { document.body.scrollTop = 0; };   
 });
 
-/* Menu onClick Buttons */
+// Menu onClick Buttons 
 function scrollTo(elementID) {
     $('html, body').animate({
         scrollTop: $("#" + elementID).offset().top
     }, 1000);
 };
+
+// Google Analytics Tracking
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-90372061-1', 'auto');
+ga('send', 'pageview');
