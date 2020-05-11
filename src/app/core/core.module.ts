410389b878a4186e742f962b9a3af52144f88f8e
Thomas Ortiz <ThomasOrtiz95@gmail.com>
2020-05-10 20:58:16 -0400

    Refactored dependencies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,30 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { SharedModule } from '../shared/shared.module';
 import { NavComponent } from './nav/nav.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  imports: [SharedModule, RouterModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    LayoutModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule],
+  imports: [SharedModule, RouterModule],
   exports: [NavComponent, PageNotFoundComponent],
   declarations: [NavComponent, PageNotFoundComponent],
   providers: [],
